48227f429fd0cb3384671e3b4af56c4daf9fadf4
UserBryan <bkk@gmail.com>
2023-11-11 00:53:51 -0800

    Main c file

---
+++ main.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+
+/**
+ * main - This function is the entry point
+ * @argc: This parameter is the arg count
+ * @argv: This parameter is the arg vector
+ *
+ * Return: 0 if success, 1 there is error
+ */
+
+int main(int argc, char **argv)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+			{
+				exit(126);
+			}
+			if (errno == ENOENT)
+			{
+				_eputs(argv[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(argv[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_the_history(info);
+	hsh(info, argv);
+	return (EXIT_SUCCESS);
+}
