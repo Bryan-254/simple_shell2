1863132c963a0969f00688e05e2316936b5a35d4
UserBryan <bkk@gmail.com>
2023-11-12 07:22:40 -0800

    Built-in 1 & 2

---
+++ built_in.c
@@ -0,0 +1,99 @@
+#include "shell.h"
+
+/**
+ * _myexitshell - This function exits the shell
+ * @info: This ia a structure containing potential arguments.
+ * Return: exits with a given exit status
+ */
+
+int _myexitshell(info_t *info)
+{
+	int exit_check_var;
+
+	if (info->argv[1])  /* If there is an exit arguement */
+	{
+		exit_check_var = _erratoi(info->argv[1]);
+		if (exit_check_var == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * _mycdshell - This function changes process' current directory
+ * @info: This is a structure containing potential arguments.
+ *  Return: 0
+ */
+
+int _mycdshell(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_returnvalue;
+
+	s = getcwd(buffer, 1024);
+	if (s == NULL)
+		_puts("TODO: >>getcwd failure msg displayed here<<\n");
+	if ((info->argv[1]) == NULL)
+	{
+		dir = _getenv(info, "HOME=");
+		if (dir == NULL)
+			chdir_returnvalue = /* TODO: this should be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_returnvalue = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if ((_getenv(info, "OLDPWD=")) == NULL)
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_returnvalue = /* TODO: this should be? */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_returnvalue = chdir(info->argv[1]);
+	if (chdir_returnvalue == -1)
+	{
+		print_error(info, "can't change directory to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _myhelp - This function changes current directory of process
+ * @info: This is a structure containing potential arguments.
+ *  Return: 0
+ */
+
+int _myhelp(info_t *info)
+{
+	char **the_arg_array;
+
+	the_arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+
+	if (0)
+	{
+		_puts(*the_arg_array); /* temp att_unused workaround */
+	}
+	return (0);
+}
---
+++ built_in2.c
@@ -0,0 +1,130 @@
+#include "shell.h"
+
+/**
+ * _myhistorylist - This function displays history list, one command by line
+ * @info: This is a structure containing potential arguments.
+ *  Return: 0
+ */
+
+int _myhistorylist(info_t *info)
+{
+	print_the_list(info->history);
+	return (0);
+}
+
+/**
+ * unset_alias - This function sets the alias to string
+ * @info: This is a parameter struct
+ * @str: the string alias
+ * Return: Always 0 on success, 1 on error
+ */
+
+int unset_alias(info_t *info, char *str)
+{
+	char *char_point, c;
+	int ret_value;
+
+	char_point = _strchr(str, '=');
+
+	if (char_point == NULL)
+	{
+		return (1);
+	}
+	c = *char_point;
+	*char_point = 0;
+	ret_value = delete_node_at_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*char_point = c;
+	return (ret_value);
+}
+
+/**
+ * set_alias - This function sets alias to string
+ * @info: This is a parameter struct
+ * @str: This is the string alias
+ *
+ * Return: 0 if successful, 1 if error
+ */
+
+int set_alias(info_t *info, char *str)
+{
+	char *char_p;
+
+	char_p = _strchr(str, '=');
+	if (char_p == NULL)
+	{
+		return (1);
+	}
+	if (*++char_p == NULL)
+	{
+		return (unset_alias(info, str));
+	}
+
+	unset_alias(info, str);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
+}
+
+/**
+ * print_alias - This function prints an alias string
+ * @node: This is the alias node
+ *
+ * Return: 0 if successful, 1 if error
+ */
+
+int print_alias(list_t *node)
+{
+	char *char_p = NULL, *x = NULL;
+
+	if (node)
+	{
+		char_p = _strchr(node->str, '=');
+
+		for (x = node->str; x <= char_p; x++)
+		{
+			_putchar(*x);
+		}
+		_putchar('\'');
+		_puts(char_p + 1);
+		_puts("'\n");
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * _myalias - This function mimics the alias builtin, man alias
+ * @info: This is a structure containing potential arguments.
+ *  Return: 0
+ */
+
+int _myalias(info_t *info)
+{
+	int x = 0;
+	char *char_p = NULL;
+	list_t *list_node = NULL;
+
+	if (info->argc == 1)
+	{
+		list_node = info->alias;
+		while (list_node)
+		{
+			print_alias(list_node);
+			list_node = list_node->next;
+		}
+		return (0);
+	}
+	for (x = 1; info->argv[x]; x++)
+	{
+		char_p = _strchr(info->argv[x], '=');
+		if (char_p)
+		{
+			set_alias(info, info->argv[x]);
+		}
+		else
+		{
+			print_alias(node_starts_with(info->alias, info->argv[x], '='));
+		}
+	}
+
+	return (0);
+}
